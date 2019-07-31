bad2560c6a89b2ddc6c2b3a811ff7a0630602279
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2019-07-31 08:11:10 -0300

    Move tests.py into a tests package.

--- src/tests/test_timed_step.py
+++ src/tests/test_timed_step.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+
 import datetime
 
 from slugify import slugify
